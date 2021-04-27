cdd1bcccd25d0328cab479685f3e024eb744da63
Dan-Claudiu Gavril <dcgavril@users.noreply.github.com>
2021-04-27 22:42:42 +0200

    Code style

--- Returns information from child Site/LiteSpeed Cache/litespeed-cache.php
+++ Returns information from child Site/LiteSpeed Cache/litespeed-cache.php
@@ -1,6 +1,6 @@
 <?php
-if(has_action('litespeed_purge_all')) { 
-	do_action('litespeed_purge_all');
+if ( has_action( 'litespeed_purge_all' ) ) { 
+  do_action( 'litespeed_purge_all' );
   echo "LiteSpeed Cache cleared successfully!", PHP_EOL;
 } else {
   echo "LiteSpeed Cache not installed", PHP_EOL;
